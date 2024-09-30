7dc62ef10ff49b13f66d14cc83619f7a03475859
Angran Li <a.li@student.fdu.edu>
2024-09-29 23:17:35 -0700

    1. Operation column 2. pagination

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -18,9 +18,11 @@
     </el-header>
     <div class="product-list">
       <div>Product List</div>
-      <el-table :data="productList" stripe border  style="width: 100%">
-        <el-table-column prop="name" label="Product Name" width="180" align="center" header-align="center"></el-table-column>
-        <el-table-column prop="category" label="Category" width="120" align="center" header-align="center"></el-table-column>
+      <el-table :data="productList" stripe border style="width: 100%">
+        <el-table-column prop="name" label="Product Name" width="180" align="center"
+          header-align="center"></el-table-column>
+        <el-table-column prop="category" label="Category" width="120" align="center"
+          header-align="center"></el-table-column>
         <el-table-column prop="brand" label="Brand" width="120" align="center" header-align="center"></el-table-column>
         <el-table-column prop="price" label="Price" width="100" align="center" header-align="center">
           <template slot-scope="scope">
@@ -28,14 +30,26 @@
           </template>
         </el-table-column>
         <el-table-column prop="description" label="Description" align="center" header-align="center"></el-table-column>
-        <el-table-column prop="material" label="Material" width="120" align="center" header-align="center"></el-table-column>
-        <el-table-column prop="stockQuantity" label="Stock Quantity" width="120" align="center" header-align="center"></el-table-column>
+        <el-table-column prop="material" label="Material" width="120" align="center"
+          header-align="center"></el-table-column>
+        <el-table-column prop="stockQuantity" label="Stock Quantity" width="120" align="center"
+          header-align="center"></el-table-column>
         <el-table-column prop="rating" label="Rating" width="100" align="center" header-align="center">
           <template slot-scope="scope">
             <el-rate v-model="scope.row.rating" disabled show-score></el-rate>
           </template>
         </el-table-column>
+        <el-table-column label="Operation" align="center" width="130">
+          <template slot-scope="scope">
+            <el-button @click="goToDetail(scope.row)" type="text" size="small"
+              style="text-decoration: underline;">Detail</el-button>
+            <el-button type="text" size="small" style="text-decoration: underline;">Add to Cart</el-button>
+          </template>
+        </el-table-column>
       </el-table>
+      <el-pagination background layout="prev, pager, next" :total="totalItems" :page-size="pageSize"
+        :current-page.sync="currentPage" @current-change="getProductList">
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -45,56 +59,69 @@ export default {
   data() {
     return {
       username: "",
-      productList: []
+      productList: [],
+      totalItems: 0,
+      currentPage: 1,
+      pageSize: 10,
+
     };
   },
   created() {
     this.username = localStorage.getItem('user');
     this.getProductList();
     this.productList = [
-        {
-          name: '智能手机',
-          category: '电子产品',
-          brand: 'Brand A',
-          price: 4999,
-          description: '一款高性能智能手机',
-          material: '金属',
-          stockQuantity: 50,
-          rating: 4.5
-        },
-        {
-          name: '笔记本电脑',
-          category: '电子产品',
-          brand: 'Brand B',
-          price: 8999,
-          description: '轻薄型笔记本电脑',
-          material: '铝合金',
-          stockQuantity: 30,
-          rating: 4.8
-        },
-        {
-          name: '运动鞋',
-          category: '服装',
-          brand: 'Brand C',
-          price: 599,
-          description: '透气舒适运动鞋',
-          material: '织物',
-          stockQuantity: 100,
-          rating: 4.2
-        },
-      ]
+      {
+        name: '智能手机',
+        category: '电子产品',
+        brand: 'Brand A',
+        price: 4999,
+        description: '一款高性能智能手机',
+        material: '金属',
+        stockQuantity: 50,
+        rating: 4.5
+      },
+      {
+        name: '笔记本电脑',
+        category: '电子产品',
+        brand: 'Brand B',
+        price: 8999,
+        description: '轻薄型笔记本电脑',
+        material: '铝合金',
+        stockQuantity: 30,
+        rating: 4.8
+      },
+      {
+        name: '运动鞋',
+        category: '服装',
+        brand: 'Brand C',
+        price: 599,
+        description: '透气舒适运动鞋',
+        material: '织物',
+        stockQuantity: 100,
+        rating: 4.2
+      },
+    ]
   },
   methods: {
     getProductList() {
-      this.$axios.get('/Instrument/getInstrumentList')
+      this.$axios.get('/Instrument/getInstrumentList', {
+        params: {
+          page: this.currentPage,
+          pageSize: this.pageSize,
+        }
+      })
         .then(response => {
           console.log(response.data);
+          this.productList = response.data.productList;
+          this.totalItems = response.data.total;
         })
         .catch(error => {
           console.error(error);
         });
-
-    }
+    },
+    goToDetail(row) {
+      console.log(row);
+    },
   }
 };
 </script>
@@ -124,7 +151,12 @@ export default {
 .el-table {
   margin-top: 20px;
 }
-.product-list{
+
+.product-list {
   padding: 10px 40px;
+}
+
+.el-pagination {
+  margin-top: 20px;
 }
 </style>
