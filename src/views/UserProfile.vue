f31c663ec8e9c06b10d901c2a5f8a7a53aefdab2
DongyeLiu <dl3386@columbia.edu>
2024-11-18 18:21:49 -0800

    edit address, email, or phone number of account

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,30 +1,43 @@
 <template>
   <div class="container">
     <el-card class="card" shadow="hover">
-      <h2 class="title">user profile</h2>
-      <div class="content">
+      <h2 class="title">User Profile</h2>
+      <div v-if="loading" class="loading">Loading...</div>
+      <div v-else class="content">
         <div>
-          <span>Username:</span>
+          <span>Username: </span>
           <span>{{ info.username }}</span>
         </div>
         <div>
-          <span>Type:</span>
+          <span>Type: </span>
           <span>{{ info.role }}</span>
         </div>
         <div>
-          <span>Email:</span>
-          <span>{{ info.email }}</span>
+          <span>Email: </span>
+          <span v-if="!editMode">{{ info.email }}</span>
+          <el-input v-else v-model="tempInfo.email" size="small"></el-input>
         </div>
         <div>
-          <span>Phone:</span>
-          <span>{{ info.phoneNumber }}</span>
+          <span>Phone: </span>
+          <span v-if="!editMode">{{ info.phoneNumber }}</span>
+          <el-input v-else v-model="tempInfo.phoneNumber" size="small"></el-input>
         </div>
         <div>
-          <span>Address:</span>
-          <span>{{ info.address }}</span>
+          <span>Address: </span>
+          <span v-if="!editMode">{{ info.address }}</span>
+          <el-input v-else v-model="tempInfo.address" size="small"></el-input>
         </div>
       </div>
-      <el-button class="button" type="primary" @click="handleClick">Go back</el-button>
+      <div class="actions">
+        <div class="left-buttons">
+          <el-button v-if="!editMode" type="primary" @click="editMode = true">Edit</el-button>
+          <el-button v-if="editMode" type="success" @click="saveChanges">Save</el-button>
+          <el-button v-if="editMode" type="danger" @click="cancelChanges">Cancel</el-button>
+        </div>
+        <div class="right-buttons">
+          <el-button class="button" type="primary" @click="handleClick">Go back</el-button>
+        </div>
+      </div>
     </el-card>
   </div>
 </template>
@@ -39,47 +52,75 @@ export default {
         email: "",
         address: "",
         phoneNumber: ""
-      }
+      },
+      tempInfo: {}, // Temporary object to hold editable fields
+      loading: true,
+      editMode: false, // Tracks whether we are in edit mode
     };
   },
   created() {
-    this.$axios.post('/UserProfile/getUserInfoByUsername', {
-      username: localStorage.getItem('user')
-    })
-      .then(response => {
-        console.log(response.data);
-        let res = response.data;
-        if (res['code'] == 200) {
-          this.info.username = res.username;
-          this.info.role = res.role;
-          this.info.email = res.email;
-          this.info.address = res.address;
-          this.info.phoneNumber = res.phoneNumber;
-        }
-        else {
-          this.$message({
-            message: 'Operation failed',
-            type: 'error'
-          });
-        }
-
-      })
-      .catch(error => {
-        console.error(error);
-        this.$message({
-          message: 'Operation failed',
-          type: 'error'
-        });
-      });
-
+    this.fetchUserInfo();
   },
   methods: {
+    async fetchUserInfo() {
+      try {
+        const response = await this.$axios.post('/UserProfile/getUserInfoByUsername', {
+          username: localStorage.getItem('user')
+        });
+        const res = response.data;
+        if (res.code === 200) {
+          this.info = { ...res };
+        } else {
+          this.$message.error('Failed to fetch user information');
+        }
+      } catch (error) {
+        console.error(error);
+        this.$message.error('An error occurred while fetching user information');
+      } finally {
+        this.loading = false;
+      }
+    },
+    editModeToggle() {
+      this.tempInfo = { ...this.info }; // Copy current info to tempInfo for editing
+      this.editMode = true;
+    },
+    async saveChanges() {
+      try {
+        const response = await this.$axios.post('/UserProfile/updateUserInfo', {
+            username: this.info.username, // Ensure username is included in the payload
+            email: this.tempInfo.email,
+            phoneNumber: this.tempInfo.phoneNumber,
+            address: this.tempInfo.address,
+        });
+        if (response.data.code === 200) {
+          this.info = { 
+                ...this.info, 
+                email: this.tempInfo.email,
+                phoneNumber: this.tempInfo.phoneNumber,
+                address: this.tempInfo.address 
+            };
+          this.editMode = false;
+          this.$message.success('Profile updated successfully');
+        } else {
+          this.$message.error('Failed to save changes');
+        }
+      } catch (error) {
+        console.error(error);
+        this.$message.error('An error occurred while saving changes');
+      }
+    },
+    cancelChanges() {
+      this.tempInfo = {}; // Clear tempInfo
+      this.editMode = false; // Exit edit mode without saving
+    },
     handleClick() {
       this.$router.back();
     }
   }
 };
 </script>
+
+
 
 <style scoped>
 .container {
@@ -112,9 +153,22 @@ export default {
   padding: 10px 20px;
 }
 
+.left-buttons,
+.right-buttons {
+  display: flex;
+  gap: 10px; /* Adds space between buttons in each group */
+}
+
 .content>div {
   margin-top: 20px;
   margin-bottom: 50px;
   font-weight: bold;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between; /* Ensures space between groups */
+  align-items: center;
+  margin-top: 20px;
 }
 </style>
